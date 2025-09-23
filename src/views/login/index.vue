6df86058a9dce1ea104890057e09d25477f1aae5
huyuelong <949910047@qq.com>
2025-09-24 00:45:02 +0800

    feat(login): 登录页静态搭建

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,63 @@
 <template>
-  <div>
-    <h1>我是一级路由-登录</h1>
-  </div>
+    <div class="login-container">
+        <el-row>
+            <el-col :span="12" :xs="0"></el-col>
+            <el-col :span="12" :xs="24">
+                <el-form class="login_form">
+                    <h1>Hello</h1>
+                    <h2>欢迎来到硅谷甄选</h2>
+                    <el-form-item>
+                        <el-input :prefix-icon="User" v-model="loginForm.username" placeholder="请输入用户名"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-input :prefix-icon="Lock" v-model="loginForm.password" show-password type="password" placeholder="请输入密码"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button class="login_btn" type="primary">登录</el-button>
+                    </el-form-item>
+                </el-form>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { User, Lock } from '@element-plus/icons-vue';
+import { ref } from 'vue';
 
-<style scoped></style>
+const loginForm = ref({
+    username: 'admin',
+    password: '111111'
+});
+</script>
+
+<style scoped lang="scss">
+.login-container {
+    width: 100vw;
+    height: 100vh;
+    background: url("@/assets/images/background.jpg") no-repeat;
+    background-size: cover;
+
+    .login_form {
+        position: relative;
+        width: 80%;
+        top: 30vh;
+        background: url("@/assets/images/login_form.png") no-repeat;
+        background-size: cover;
+        padding: 40px;
+
+        h1 { 
+            color: white;
+            font-size: 40px;
+        }
+        h2 {
+            color: white;
+            font-size: 20px;
+            margin: 20px 0;
+        }
+        .login_btn { 
+            width: 100%;
+        }
+    }
+}
+</style>
