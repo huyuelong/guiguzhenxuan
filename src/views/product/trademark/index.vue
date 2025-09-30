afaf524d801eb2b01085df9dc4dc071ce43a6b2c
huyuelong <949910047@qq.com>
2025-10-01 01:56:42 +0800

    feat(product): 添加品牌管理对话框及交互功能

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <el-card class="box-card">
-            <el-button type="primary" size="default" icon="Plus">添加品牌</el-button>
+            <el-button type="primary" size="default" icon="Plus" @click="addTrademark">添加品牌</el-button>
             <!-- 表格组件 -->
             <el-table style="margin: 10px 0px;" border :data="trademarkList">
                 <el-table-column label="序号" width="80px" align="center" type="index"></el-table-column>
@@ -32,13 +32,37 @@
                 :disabled="disabled" :background="background" layout="prev, pager, next, jumper, ->, sizes, total"
                 :total="total" @size-change="handleSizeChange" @current-change="getTrademarkList" />
         </el-card>
+        <!-- 添加品牌对话框 -->
+        <el-dialog v-model="dialogFormVisible" :title="trademarkParams.id ? '修改品牌' : '添加品牌'">
+            <el-form style="width: 80%;" :model="trademarkParams" :rules="rules" ref="formRef">
+                <el-form-item label="品牌名称" label-width="100px" prop="tmName">
+                    <el-input placeholder="请您输入品牌名称" v-model="trademarkParams.tmName"></el-input>
+                </el-form-item>
+                <el-form-item label="品牌LOGO" label-width="100px" prop="logoUrl">
+                    <!-- upload组件属性:action图片上传路径书写/api,代理服务器不发送这次post请求  -->
+                    <el-upload class="avatar-uploader" action="/api/admin/product/fileUpload" :show-file-list="false"
+                        :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+                        <img v-if="trademarkParams.logoUrl" :src="trademarkParams.logoUrl" class="avatar" />
+                        <el-icon v-else class="avatar-uploader-icon">
+                            <Plus />
+                        </el-icon>
+                    </el-upload>
+
+                </el-form-item>
+            </el-form>
+            <!-- 具名插槽:footer -->
+            <template #footer>
+                <el-button type="primary" size="default" @click="cancel">取消</el-button>
+                <el-button type="primary" size="default" @click="confirm">确定</el-button>
+            </template>
+        </el-dialog>
     </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 import { reqHasTrademark, reqAddOrUpdateTrademark, reqDeleteTrademark } from '@/apis/product/trademark/index';
-import { TradeMarkResponseData } from '@/apis/product/trademark/type';
+import { TradeMark, TradeMarkResponseData } from '@/apis/product/trademark/type';
 
 const pageNo = ref<number>(1);
 const pageSize = ref<number>(10);
@@ -48,7 +72,9 @@ const disabled = ref<boolean>(false);
 const background = ref<boolean>(true);
 const total = ref<number>(0);
 const trademarkList = ref<any[]>([]);
+const dialogFormVisible = ref<boolean>(false);
 
+// 获取品牌列表
 const getTrademarkList = async (pager = 1) => {
     pageNo.value = pager;
     const res: TradeMarkResponseData = await reqHasTrademark(pageNo.value, pageSize.value);
@@ -62,10 +88,29 @@ onMounted(() => {
     getTrademarkList();
 });
 
+// 分页器:size-change
 const handleSizeChange = (val: number) => {
     console.log(val);
     getTrademarkList();
 };
+
+// 添加品牌
+const addTrademark = () => { 
+    dialogFormVisible.value = true;
+};
+
+// 修改品牌
+const updateTrademark = (row: TradeMark) => {
+    dialogFormVisible.value = true;
+}
+
+const cancel = () => {
+    dialogFormVisible.value = false;
+}
+
+const confirm = async () => { 
+    dialogFormVisible.value = false;
+}
 
 </script>
 
