a58c853d59a93fa6a8078c91f706c98c70d05a89
huyuelong <949910047@qq.com>
2025-10-06 22:56:53 +0800

    feat(category): 实现三级分类联动选择功能

--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <!-- 三级分类全局组件 -->
-        <Category />
+        <Category :scene="scene" />
         <el-card style="margin: 10px 0px;">
-            <el-button type="primary" icon="Plus">添加属性</el-button>
+            <el-button type="primary" icon="Plus" :disabled="categoryStore.c3Id ? false : true">添加属性</el-button>
             <el-table border style="margin: 10px 0px">
                 <el-table-column label="序号" type="index" align="center" width="80px"></el-table-column>
                 <el-table-column label="属性名称" width="120px"></el-table-column>
@@ -15,7 +15,12 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import { useCategoryStore } from '@/store/modules/category';
 
+const categoryStore = useCategoryStore();
+
+const scene = ref<number>(0); //scene=0,显示table,scene=1,展示添加与修改属性结构
 </script>
 
 <style scoped lang="scss">
